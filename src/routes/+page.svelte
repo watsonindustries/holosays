93baefc4eedf39becd12f06de1e1511a9c611c18
Daniils Petrovs <daniils@platogo.com>
2023-04-09 16:31:21 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Welcome to the HoloSays Beta</h1>
+      <p class="py-6">The number one source of livestream transcripts!</p>
+      <button class="btn btn-primary rounded-full">Get Started</button>
+    </div>
+  </div>
+</div>
